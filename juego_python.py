4cf3308ef58ba9d0660ccde9915af5b62970a4ca
lina1712 <dolliu0915@gmail.com>
2024-09-08 13:34:53 -0500

    fix + function window score

--- juego_python.py
+++ juego_python.py
@@ -10,6 +10,7 @@ from bala import Bala
 from premio import Premio
 
 import random
+import sys
 
 
 pygame.init()
@@ -21,162 +22,237 @@ ANCHO = 1000
 ALTO = 720
 VENTANA = pygame.display.set_mode([ANCHO,ALTO])
 FPS = 60
+pygame.display.set_caption("Mi juego")
+
 FUENTE = pygame.font.SysFont("Cascadia code", 40)
 SONIDO_DISPARO = pygame.mixer.Sound('C:/Users/dolli/Documents/GitHub/game_one/laser.mp3')
 SONIDO_MUERTE_ENEMIGO = pygame.mixer.Sound('C:/Users/dolli/Documents/GitHub/game_one/muerte_enemigo.mp3')
 SONIDO_MUERTE_PERSONAJE = pygame.mixer.Sound('C:/Users/dolli/Documents/GitHub/game_one/muerte_personaje.mp3')
 
-#variables
-jugando = True
 
-reloj = pygame.time.Clock()
 
-vida = 3
+def pantalla_inicio():
+    ventana_inicio = True
+    while ventana_inicio:
+        VENTANA.fill("black")
+        texto_inicio = FUENTE.render("Presiona ENTER para comenzar", True, "White")
+        VENTANA.blit(texto_inicio, (ANCHO // 2 - texto_inicio.get_width() // 2, ALTO // 2 - texto_inicio.get_height() // 2))
+        pygame.display.update()
 
-puntos = 0
+        for evento in pygame.event.get():
+            if evento.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if evento.type == pygame.KEYDOWN:
+                if evento.key == pygame.K_RETURN:
+                    ventana_inicio = False
 
-tiempo_pasado = 0
-tiempo_entre_enemigos = 800
 
-tiempo_pasado_premios = 0
-tiempo_entre_premios = 2000
+def pantalla_puntajes(puntos):
+    nombre = ""
 
-cubo = Cubo(ANCHO/2, ALTO-75)
+    #crear la ventana para mostrar los puntajes
+    ventana_puntajes = True
+    while ventana_puntajes:
+        VENTANA.fill("black")
 
-enemigos = []
+        #mostar mensaje de ingreso de nombre
+        texto_nombre = FUENTE.render(f"Introduce tu nombre :{nombre}", True, "White")
+        VENTANA.blit(texto_nombre, (ANCHO // 2 - texto_nombre.get_width() // 2, 50))
 
-premios = []
+        #mostrar puntajes
+        texto_puntajes = FUENTE.render("Puntajes:", True, "White")
+        VENTANA.blit(texto_puntajes, (ANCHO // 2 - texto_puntajes.get_width() // 2, 150))
 
-balas = []
 
-ultima_bala = 0
+        #leer todas la spuntuaciones
+        with open('puntuaciones.txt', 'r') as archivo:
+            puntuaciones = archivo.readlines()
 
-tiempo_entre_balas = 600
+        puntuaciones.sort(reverse=True, key=lambda x: int(x.split(":")[1].strip()))
 
-enemigos.append(Enemigo(ANCHO/2, 100))
+        #mostar las primeras puntuaciones
+        for i, puntaje in enumerate(puntuaciones[:5]):
+            texto_puntaje = FUENTE.render(puntaje.strip(), True, "White")
+            VENTANA.blit(texto_puntaje, (ANCHO // 2 - texto_puntaje.get_width() // 2, 100 + i * 40 ))
 
-premios.append(Premio(ANCHO/2, 100))
+        texto_reiniciar = FUENTE.render("Presiona ESPACIO para volver a jugar", True, "White")
+        VENTANA.blit(texto_reiniciar, (ANCHO // 2 - texto_reiniciar.get_width() // 2, ALTO - 100))
 
-def crear_bala():
-    global ultima_bala
+        pygame.display.update()
 
-    if pygame.time.get_ticks() - ultima_bala > tiempo_entre_balas:
-        balas.append(Bala(cubo.rect.centerx, cubo.rect.centery))
-        ultima_bala = pygame.time.get_ticks()
-        SONIDO_DISPARO.play()
+        #eventos para salir o volver a jugar
+        for evento in pygame.event.get():
+            if evento.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
 
-def gestionar_teclas(teclas):
-    #if teclas[pygame.K_w]:
-     #   cubo.y -= cubo.velocidad
-    #if teclas[pygame.K_s]:
-      #  cubo.y += cubo.velocidad
-    if teclas[pygame.K_a]:
-        if cubo.x >= 0:
-            cubo.x -= cubo.velocidad
-    if teclas[pygame.K_d]:
-        if cubo.x + cubo.ancho <= ANCHO:
-            cubo.x += cubo.velocidad
-    if teclas[pygame.K_SPACE]:
-        crear_bala()
+            if evento.type == pygame.KEYDOWN:
+                if evento.key == pygame.K_BACKSPACE:
+                    nombre = nombre[:-1]
+                elif evento.key == pygame.K_RETURN:
+                    with open('puntuaciones.txt', 'a') as archivo:
+                        archivo.write(f"{nombre} : {puntos}\n")
+                    ventana_puntajes = False
+                elif evento.key == pygame.K_SPACE and len(nombre) > 0:
+                    ventana_puntajes =False
+                else:
+                    if len(nombre) < 10:
+                        nombre += evento.unicode
 
-#creando un bucle para que la ventana quede permanente mientras jugamos
-while jugando and vida > 0:
+def juego():
 
-    tiempo_pasado += reloj.tick(FPS)
+    while True:
+        pantalla_inicio()
 
-    if tiempo_pasado > tiempo_entre_enemigos:
-        enemigos.append(Enemigo(random.randint(0,ANCHO), -100))
+        #variables
+        jugando = True
+
+        reloj = pygame.time.Clock()
+
+        vida = 3
+
+        puntos = 0
+
         tiempo_pasado = 0
+        tiempo_entre_enemigos = 800
 
-    tiempo_pasado_premios += reloj.tick(FPS)
-
-    if tiempo_pasado_premios > tiempo_entre_premios:
-        premios.append(Premio(random.randint(0,ANCHO), -100))
         tiempo_pasado_premios = 0
+        tiempo_entre_premios = 2000
 
-    eventos = pygame.event.get()
+        cubo = Cubo(ANCHO/2, ALTO-75)
 
-    teclas = pygame.key.get_pressed()
+        enemigos = []
 
-    texto_vida = FUENTE.render(f"Vida: {vida}", True, "White")
+        premios = []
 
-    texto_puntos = FUENTE.render(f"Puntos: {puntos}", True, "White")
+        balas = []
 
-    gestionar_teclas(teclas)
+        ultima_bala = 0
 
-    for evento in eventos:
-        if evento.type == pygame.QUIT:
-            jugando = False
+        tiempo_entre_balas = 600
 
-    VENTANA.fill ("purple")
+        enemigos.append(Enemigo(ANCHO/2, 100))
 
-    cubo.dibujar(VENTANA)
+        premios.append(Premio(ANCHO/2, 100))
 
-    for enemigo in enemigos:
-        enemigo.dibujar(VENTANA)
-        enemigo.movimiento()
 
-        if pygame.Rect.colliderect(cubo.rect, enemigo.rect):
-            vida -= 1
-            SONIDO_MUERTE_PERSONAJE.play()
-            print(f'Te quedan {vida} vidas')
-            enemigos.remove(enemigo)
+        def crear_bala():
+            nonlocal ultima_bala
 
-        if enemigo.y > ALTO:
-            puntos -= 1
-            enemigos.remove(enemigo)
+            if pygame.time.get_ticks() - ultima_bala > tiempo_entre_balas:
+                balas.append(Bala(cubo.rect.centerx, cubo.rect.centery))
+                ultima_bala = pygame.time.get_ticks()
+                SONIDO_DISPARO.play()
 
-        for bala in balas:
-            if pygame.Rect.colliderect(bala.rect, enemigo.rect):
-                enemigo.vida -= 1
-                balas.remove(bala)
+        def gestionar_teclas(teclas):
+            #if teclas[pygame.K_w]:
+            #   cubo.y -= cubo.velocidad
+            #if teclas[pygame.K_s]:
+            #  cubo.y += cubo.velocidad
+            if teclas[pygame.K_a]:
+                if cubo.x >= 0:
+                    cubo.x -= cubo.velocidad
+            if teclas[pygame.K_d]:
+                if cubo.x + cubo.ancho <= ANCHO:
+                    cubo.x += cubo.velocidad
+            if teclas[pygame.K_SPACE]:
+                crear_bala()
 
-        if enemigo.vida <= 0:
-            SONIDO_MUERTE_ENEMIGO.play()
-            enemigos.remove(enemigo)
-            puntos += 3
+    #creando un bucle para que la ventana quede permanente mientras jugamos
+        while jugando and vida > 0:
 
-    for bala in balas:
-        bala.dibujar(VENTANA)
-        bala.movimiento()
+            tiempo_pasado += reloj.tick(FPS)
 
-        if bala.y < 0:
-            balas.remove(bala)
+            if tiempo_pasado > tiempo_entre_enemigos:
+                enemigos.append(Enemigo(random.randint(0,ANCHO), -100))
+                tiempo_pasado = 0
 
-    for premio in premios:
-        premio.dibujar(VENTANA)
-        premio.movimiento()
+            tiempo_pasado_premios += reloj.tick(FPS)
 
-        if pygame.Rect.colliderect(premio.rect, cubo.rect):
-            premios.remove(premio)
+            if tiempo_pasado_premios > tiempo_entre_premios:
+                premios.append(Premio(random.randint(0,ANCHO), -100))
+                tiempo_pasado_premios = 0
 
-            if premio.tipo == 1:
-                if vida <= 5:
-                    vida += 1
-                else:
-                    puntos += 10
+            eventos = pygame.event.get()
 
-            elif premio.tipo == 2:
-                if tiempo_entre_balas > 200:
-                    tiempo_entre_balas /= 2
-                else:
-                    puntos +=10
+            teclas = pygame.key.get_pressed()
 
-        if premio.y > ALTO:
-            premios.remove(premio)
+            texto_vida = FUENTE.render(f"Vida: {vida}", True, "White")
 
-    VENTANA.blit(texto_vida, (20, 20))
-    VENTANA.blit(texto_puntos, (20, 50))
+            texto_puntos = FUENTE.render(f"Puntos: {puntos}", True, "White")
 
-    pygame.display.update()
+            gestionar_teclas(teclas)
 
-#SONIDO_MUERTE.play()
+            for evento in eventos:
+                if evento.type == pygame.QUIT:
+                    jugando = False
+
+            VENTANA.fill ("purple")
+
+            cubo.dibujar(VENTANA)
+
+            for enemigo in enemigos:
+                enemigo.dibujar(VENTANA)
+                enemigo.movimiento()
+
+                if pygame.Rect.colliderect(cubo.rect, enemigo.rect):
+                    vida -= 1
+                    SONIDO_MUERTE_PERSONAJE.play()
+                    print(f'Te quedan {vida} vidas')
+                    enemigos.remove(enemigo)
+
+                if enemigo.y > ALTO:
+                    puntos -= 1
+                    enemigos.remove(enemigo)
+
+                for bala in balas:
+                    if pygame.Rect.colliderect(bala.rect, enemigo.rect):
+                        enemigo.vida -= 1
+                        balas.remove(bala)
+
+                if enemigo.vida <= 0:
+                    SONIDO_MUERTE_ENEMIGO.play()
+                    enemigos.remove(enemigo)
+                    puntos += 3
+
+            for bala in balas:
+                bala.dibujar(VENTANA)
+                bala.movimiento()
+
+                if bala.y < 0:
+                    balas.remove(bala)
+
+            for premio in premios:
+                premio.dibujar(VENTANA)
+                premio.movimiento()
+
+                if pygame.Rect.colliderect(premio.rect, cubo.rect):
+                    premios.remove(premio)
+
+                    if premio.tipo == 1:
+                        if vida < 3:
+                            vida += 1
+                        else:
+                            puntos += 10
+
+                    elif premio.tipo == 2:
+                        if tiempo_entre_balas > 200:
+                            tiempo_entre_balas /= 2
+                        else:
+                            puntos +=10
+
+                if premio.y > ALTO:
+                    premios.remove(premio)
+
+            VENTANA.blit(texto_vida, (20, 20))
+            VENTANA.blit(texto_puntos, (20, 50))
+
+            pygame.display.update()
+
+        if not pantalla_puntajes(puntos):
+            break
+        #SONIDO_MUERTE.play()
+
+juego()
 pygame.quit()
-
-nombre = input("Introduce tu nombre : ")
-
-with open('puntuaciones.txt', 'a') as archivo:
-    archivo.write(f"{nombre} : {puntos}\n")
-
-quit()
-
